fdd4e44376a0f26aeb1b4ba1d4dd0a16e19cdf9b
Rafael Neves de Oliveira <rafaelneves@MacBook-Thamiris.local>
2021-11-06 10:27:00 -0300

    first commit

---
+++ src/app/cursos/cursos-routing.module.ts
@@ -0,0 +1,13 @@
+import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: CursosListaComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CursosRoutingModule { }
