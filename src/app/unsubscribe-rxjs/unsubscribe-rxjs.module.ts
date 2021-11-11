f6389e98a1249ddd7a5719af1bfa2dd82d2d47e7
Rafael Neves <rafaelneves@MacBook-Air-de-Rafael.local>
2021-11-11 19:07:52 -0300

    add poc unsubscribe - pocs

--- src/app/unsubscribe-rxjs/unsubscribe-rxjs.module.ts
+++ src/app/unsubscribe-rxjs/unsubscribe-rxjs.module.ts
@@ -6,6 +6,9 @@ import { UnsubscribePocComponent } from './unsubscribe-poc/unsubscribe-poc.compo
 import { PocBaseComponent } from './poc-base/poc-base.component';
 import { PocAsyncComponent } from './componentes/poc-async.component';
 import { PocTakeUntilComponent } from './componentes/poc-take-until.component';
+import { PocTakeComponent } from './componentes/poc-take.component';
+import { PocUnsubComponent } from './componentes/poc-unsub.component';
+import { PocComponent } from './componentes/poc.component';
 
 
 @NgModule({
@@ -13,7 +16,10 @@ import { PocTakeUntilComponent } from './componentes/poc-take-until.component';
     UnsubscribePocComponent,
     PocBaseComponent,
     PocAsyncComponent,
-    PocTakeUntilComponent
+    PocTakeUntilComponent,
+    PocTakeComponent,
+    PocUnsubComponent,
+    PocComponent
   ],
   imports: [
     CommonModule,
